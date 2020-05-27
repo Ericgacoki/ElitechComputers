ba98455cdb769d1c7c676747174b02b5388a4fed
Eric <54077752+Ericgacoki@users.noreply.github.com>
2020-05-27 19:23:18 +0300

    Update ContactsActivity.kt

--- app/src/main/java/elitechcomps/com/ContactsActivity.kt
+++ app/src/main/java/elitechcomps/com/ContactsActivity.kt
@@ -8,5 +8,6 @@ class ContactsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
+        // add contacts 
     }
 }
