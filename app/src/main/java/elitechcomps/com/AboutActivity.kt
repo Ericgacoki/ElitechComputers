57b1d32e394c9595cdeb27cf6989757e23e2c6b0
Eric <54077752+Ericgacoki@users.noreply.github.com>
2020-08-31 02:42:41 +0300

    Update AboutActivity.kt

--- app/src/main/java/elitechcomps/com/AboutActivity.kt
+++ app/src/main/java/elitechcomps/com/AboutActivity.kt
@@ -5,7 +5,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_about.*
-// add about us and user account manage
+// add about us
 class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
